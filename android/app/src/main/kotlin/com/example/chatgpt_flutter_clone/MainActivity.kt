cb9c6f28bddd469d1857b2791a51d40c0d617f84
epocolis <my_disposable@tutanota.com>
2023-10-20 08:38:52 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/chatgpt_flutter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatgpt_flutter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/providers/models_provider.dart
@@ -0,0 +1,29 @@
+import 'package:chatgpt_flutter_clone/models/models_model.dart';
+import 'package:chatgpt_flutter_clone/services/api_service.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class ModelsProvider with ChangeNotifier {
+  // String currentModel = "text-davinci-003";
+  String currentModel = "gpt-3.5-turbo-0301";
+
+  String get getCurrentModel {
+    return currentModel;
+  }
+
+  void setCurrentModel(String newModel) {
+    currentModel = newModel;
+    notifyListeners();
+  }
+
+  List<ModelsModel> modelsList = [];
+
+  List<ModelsModel> get getModelsList {
+    return modelsList;
+  }
+
+  Future<List<ModelsModel>> getAllModels() async {
+    modelsList = await ApiService.getModels();
+    return modelsList;
+  }
+}
